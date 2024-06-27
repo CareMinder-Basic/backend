048cac82a3891b28d8495097980fe53f9bcaa5b2
이정민 <kyungheeyj45@ajou.ac.kr>
2024-06-27 20:15:00 +0900

    feat : 병원 entity 구성

---
+++ src/main/java/com/careminder/backend/model/hospital/Hospital.java
@@ -0,0 +1,18 @@
+package com.careminder.backend.model.hospital;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Hospital {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    /** 병원의 이름 */
+    private String name;
+    private String address;
+    private String businessRegistrationNumber;
+}
