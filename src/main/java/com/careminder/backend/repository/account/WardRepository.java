f75b0b8663728bcc8381ee7fff70ea26d5e6430e
이정민 <kyungheeyj45@ajou.ac.kr>
2024-06-27 20:13:38 +0900

    feat : 병동 계정 entity, repository 구성

---
+++ src/main/java/com/careminder/backend/repository/account/WardRepository.java
@@ -0,0 +1,7 @@
+package com.careminder.backend.repository.account;
+
+import com.careminder.backend.model.account.Ward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WardRepository extends JpaRepository<String, Ward> {
+}
