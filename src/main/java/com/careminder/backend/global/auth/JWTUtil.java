a41754517c3e48eb2c924165931eafb23f62e02c
이정민 <kyungheeyj45@ajou.ac.kr>
2024-07-05 17:44:08 +0900

    feat : accessToken, refreshToken 생성

--- src/main/java/com/careminder/backend/global/auth/JWTUtil.java
+++ src/main/java/com/careminder/backend/global/auth/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.careminder.backend.global.auth;
 
+import com.careminder.backend.global.response.JWTResponse;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -7,12 +8,15 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Date;
 
 @Component
 public class JWTUtil {
 
     private SecretKey secretKey;
+    private static final Long accessTokenValidTime = Duration.ofDays(15).toMillis(); // 만료시간 15일
+    private static final Long refreshTokenValidTime = Duration.ofDays(30).toMillis(); // 만료시간 30일
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
 
@@ -40,14 +44,40 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    public String createJwt(Long userId, String role, Long expiredMs) {
+    public JWTResponse createJWT(Long userId) {
+        String accessToken = createToken(userId, accessTokenValidTime);
+        String refreshToken = createToken(userId, refreshTokenValidTime);
+        return JWTResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public String createAccessToken(Long userId) {
+        return createToken(userId, accessTokenValidTime);
+    }
+
+    public String createRefreshToken(Long userId) {
+        return createToken(userId, refreshTokenValidTime);
+    }
+
+    public String createToken(Long userId, Long expiredMs) {
 
         return Jwts.builder()
                 .claim("userId", userId)
-                .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
     }
+//    public String createToken(Long userId, String role, Long expiredMs) {
+//
+//        return Jwts.builder()
+//                .claim("userId", userId)
+//                .claim("role", role)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+//                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+//                .signWith(secretKey)
+//                .compact();
+//    }
 }
